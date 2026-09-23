7f735417cfca89522c633bbf28f208e7d5518257
toshiaki_taniguchi <ta2gucity@gb3.so-net.ne.jp>
2026-09-24 01:21:54 +0900

    B-B-02: Part・Chapter・Sectionの表示順を修正

--- app/UseCases/Part/ShowAction.php
+++ app/UseCases/Part/ShowAction.php
@@ -15,7 +15,9 @@ final class ShowAction
     {
         return $part->load([
             'certification',
-            'chapters' => fn ($q) => $q->withCount('sections'),
+            'chapters' => fn ($q) => $q
+                ->ordered()
+                ->withCount('sections'),
         ]);
     }
 }
